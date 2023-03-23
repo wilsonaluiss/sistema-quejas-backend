2aa4dafcd886bc80c1080409e0fc75c7a2168dfb
Wilson <wluis@miumg.edu.gt>
2023-03-22 23:32:07 -0600

    Creacion proyecto backend sistema de quejas

---
+++ src/main/java/com/quejas/quejas/QuejasApplication.java
@@ -0,0 +1,13 @@
+package com.quejas.quejas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuejasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuejasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quejas/quejas/QuejasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quejas.quejas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuejasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
